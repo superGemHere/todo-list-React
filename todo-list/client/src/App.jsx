b7faafd8a3c2e984f1f4f7422b3f2aadfcffa792
superGemHere <122823771+superGemHere@users.noreply.github.com>
2023-11-02 15:54:57 +0200

    + almost finished

--- todo-list/client/src/App.jsx
+++ todo-list/client/src/App.jsx
@@ -1,142 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Header from './componenets/Header'
+import Footer from './componenets/Footer'
+import TodoList from './componenets/TodoList'
 
 function App() {
   return (
     <body>
 
   {/* <!-- Navigation header --> */}
-  <header className="navigation-header">
-    <span className="navigation-logo">
-      <img src="images/todo-icon.png" alt="todo-logo" />
-    </span>
-    <span className="spacer"></span>
-    <span className="navigation-description">Todo List</span>
-  </header>
+    <Header />
 
   {/* <!-- Main content --> */}
   <main className="main">
 
+    <TodoList />
     {/* <!-- Section container --> */}
-    <section className="todo-list-container">
-      <h1>Todo List</h1>
 
-      <div className="add-btn-container">
-        <button className="btn">+ Add new Todo</button>
-      </div>
-
-      <div className="table-wrapper">
-
-        {/* <!-- Loading spinner - show the load spinner when fetching the data from the server--> */}
-        <div className="loading-container">
-          <div className="loading-spinner">
-            <span className="loading-spinner-text">Loading</span>
-          </div>
-        </div>
-
-        {/* <!-- Todo list table --> */}
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="table-header-task">Task</th>
-              <th className="table-header-status">Status</th>
-              <th className="table-header-action">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-
-            {/* <!-- Todo item --> */}
-            <tr className="todo is-completed">
-              <td>Give dog a bath</td>
-              <td>Complete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-            {/* <!-- Todo item --> */}
-            <tr className="todo is-completed">
-              <td>Do laundry</td>
-              <td>Complete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-            {/* <!-- Todo item --> */}
-            <tr className="todo">
-              <td>Vacuum floor</td>
-              <td>Incomplete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-            {/* <!-- Todo item --> */}
-            <tr className="todo is-completed">
-              <td>Feed cat</td>
-              <td>Complete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-            {/* <!-- Todo item --> */}
-            <tr className="todo">
-              <td>Change light bulbs</td>
-              <td>Incomplete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-            {/* <!-- Todo item --> */}
-            <tr className="todo is-completed">
-              <td>Feed cat</td>
-              <td>Complete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-            {/* <!-- Todo item --> */}
-            <tr className="todo">
-              <td>Change light bulbs</td>
-              <td>Incomplete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-            {/* <!-- Todo item --> */}
-            <tr className="todo is-completed">
-              <td>Go to Store</td>
-              <td>Completed</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-            {/* <!-- Todo item --> */}
-            <tr className="todo">
-              <td>Fill gas tank</td>
-              <td>Incomplete</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </section>
   </main>
 
   {/* <!-- Footer --> */}
-  <footer className="footer">
-    <p>Copyright © designed by Mihail Valkov</p>
-  </footer>
-
+  <Footer />
 </body>
 
   )
---
+++ todo-list/client/src/componenets/TodoItem.jsx
@@ -0,0 +1,19 @@
+export default function TodoItem ({
+    id,
+    text,
+    isCompleted,
+    changeStatusHandler
+    }) {
+        const onChangeStatusClick = () => {
+            changeStatusHandler(id)
+        }
+    return (
+        <tr className={`todo ${isCompleted ? ' is-completed' : ''}`}>
+                <td>{text}</td>
+                <td>{isCompleted ? 'Completed' : 'Incomplete'}</td>
+                <td className="todo-action">
+                  <button onClick={onChangeStatusClick} className="btn todo-btn">Change status</button>
+                </td>
+              </tr>
+    );
+}
